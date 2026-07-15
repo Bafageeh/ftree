874771aced00426bf0d9b449015432d03579832b
Bafageeh <a.baf@live.com>
2026-07-15 21:58:33 +0300

    Expose contribution submission and status routes

--- scaffold/api/routes/api.php
+++ scaffold/api/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\HealthController;
 use App\Http\Controllers\Api\PersonController;
+use App\Http\Controllers\Api\ReviewRequestController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -10,4 +11,9 @@ Route::prefix('v1')->group(function () {
     Route::get('/people', [PersonController::class, 'index']);
     Route::get('/people/{person}', [PersonController::class, 'show']);
     Route::get('/people/{person}/lineage', [PersonController::class, 'lineage']);
+
+    Route::post('/review-requests', [ReviewRequestController::class, 'store'])
+        ->middleware('throttle:10,1');
+    Route::get('/review-requests/{trackingCode}', [ReviewRequestController::class, 'status'])
+        ->middleware('throttle:30,1');
 });
