4d3383098cee8f90577ad16d6444b93068f3bc43
Bafageeh <a.baf@live.com>
2026-07-17 15:17:23 +0300

    Make the connected lineage page the main application

--- scaffold/api/routes/web.php
+++ scaffold/api/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Web\LineageController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/app/', 302);
+Route::redirect('/', '/lineage', 302);
 Route::get('/lineage', [LineageController::class, 'index'])->name('lineage.index');
 Route::get('/lineage/{person}', [LineageController::class, 'show'])->name('lineage.show');
 Route::redirect('/backend', '/lineage', 302);
