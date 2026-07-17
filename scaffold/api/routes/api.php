2c1a515374fc6b2f68a032462c2d222ffc4c9748
Bafageeh <a.baf@live.com>
2026-07-17 11:13:41 +0300

    Add chart-edge supervisor review route

--- scaffold/api/routes/api.php
+++ scaffold/api/routes/api.php
@@ -14,16 +14,14 @@ Route::prefix('v1')->group(function () {
     Route::get('/people', [PersonController::class, 'index']);
     Route::get('/people/{person}', [PersonController::class, 'show']);
     Route::get('/people/{person}/lineage', [PersonController::class, 'lineage']);
-    Route::post('/people/{person}/review', [SupervisorReviewController::class, 'reviewPerson'])
-        ->middleware('throttle:60,1');
+    Route::post('/people/{person}/review', [SupervisorReviewController::class, 'reviewPerson'])->middleware('throttle:60,1');
 
     Route::get('/chart-readings', [ChartReadingController::class, 'index']);
     Route::get('/chart-readings-stats', [ChartReadingController::class, 'stats']);
     Route::get('/chart-edges', [ChartEdgeController::class, 'index']);
     Route::get('/chart-edges-stats', [ChartEdgeController::class, 'stats']);
+    Route::post('/chart-edges/{chartEdge}/review', [SupervisorReviewController::class, 'reviewEdge'])->middleware('throttle:60,1');
 
-    Route::post('/review-requests', [ReviewRequestController::class, 'store'])
-        ->middleware('throttle:10,1');
-    Route::get('/review-requests/{trackingCode}', [ReviewRequestController::class, 'status'])
-        ->middleware('throttle:30,1');
+    Route::post('/review-requests', [ReviewRequestController::class, 'store'])->middleware('throttle:10,1');
+    Route::get('/review-requests/{trackingCode}', [ReviewRequestController::class, 'status'])->middleware('throttle:30,1');
 });
