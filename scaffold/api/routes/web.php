6559d94d9143027ba9c816a5efa3b4613e333115
Bafageeh <a.baf@live.com>
2026-07-17 14:48:23 +0300

    Expose non-Expo prophetic lineage web page

--- scaffold/api/routes/web.php
+++ scaffold/api/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Web\LineageController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/app/', 302);
-Route::view('/backend', 'welcome');
+Route::get('/lineage', [LineageController::class, 'index'])->name('lineage.index');
+Route::get('/lineage/{person}', [LineageController::class, 'show'])->name('lineage.show');
+Route::redirect('/backend', '/lineage', 302);
