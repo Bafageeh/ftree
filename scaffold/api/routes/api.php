155171d590e6cb9bed4a5c0a5dc7f7d60ac2f6e5
Bafageeh <a.baf@live.com>
2026-07-17 23:29:45 +0300

    Add genealogy profile details and photo routes

--- scaffold/api/routes/api.php
+++ scaffold/api/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\ChartEdgeController;
 use App\Http\Controllers\Api\ChartReadingController;
 use App\Http\Controllers\Api\HealthController;
 use App\Http\Controllers\Api\PersonController;
+use App\Http\Controllers\Api\PersonProfileController;
 use App\Http\Controllers\Api\ReviewRequestController;
 use App\Http\Controllers\Api\SupervisorReviewController;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,8 @@ Route::prefix('v1')->group(function () {
     Route::get('/people/{person}', [PersonController::class, 'show']);
     Route::get('/people/{person}/lineage', [PersonController::class, 'lineage']);
     Route::patch('/people/{person}', [PersonController::class, 'update'])->middleware('throttle:60,1');
+    Route::patch('/people/{person}/profile', [PersonProfileController::class, 'update'])->middleware('throttle:60,1');
+    Route::post('/people/{person}/photo', [PersonProfileController::class, 'uploadPhoto'])->middleware('throttle:20,1');
     Route::post('/people/{person}/children', [PersonController::class, 'addChildren'])->middleware('throttle:60,1');
     Route::delete('/people/{person}', [PersonController::class, 'destroy'])->middleware('throttle:30,1');
     Route::post('/people/{person}/review', [SupervisorReviewController::class, 'reviewPerson'])->middleware('throttle:60,1');
